d0fa48053fb71717661809979eec2129317ccb4c
Karanam <MKARANAM@ets.org>
2019-01-17 05:53:57 -0500

    changes done

---
+++ src/main/java/com/example/demo/CustomHealthCheck.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomHealthCheck implements HealthIndicator {
+
+	@Override
+	public Health health() {
+		int errorCode = 0;
+		if (errorCode != 1) {
+			return Health.down().withDetail("Error Code", errorCode).build();
+		}
+		return Health.up().build();
+	}
+
+}
